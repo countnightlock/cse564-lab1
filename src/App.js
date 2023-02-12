927572188584418f5dc27f10edbc725138610859
Vivek Maurya <vmaurya@cs.stonybrook.edu>
2023-02-12 16:11:47 -0500

    Added file read functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+// App.js
+import React, { Component } from 'react';
 import './App.css';
+import BarChart from './charts/BarChart';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { fetchData } from './utils/file';
+
+const datas = [
+    [10, 30, 40, 20],
+    [10, 40, 30, 20, 50, 10],
+    [60, 30, 40, 20, 30]
+]
+var i = 0;
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {data: []};
+    }
+
+    async componentDidMount() {
+        let data = await fetchData();
+        this.setState({ data : data });
+    }
+
+    render() {
+        let { data } = this.state;
+        console.log("printing data");
+        console.log(data);
+        return (
+            <div className="App">
+                <p>
+                    {JSON.stringify(data)}
+                </p>
+            </div>
+        )
+    }
 }
+
+// function App() {
+//     const [data, setData] = useState([]);
+
+//     setData(fetchData());
+//     console.log(data);
+
+//     useEffect(() => {
+//         changeData();
+//     }, []);
+
+//     const changeData = () => {
+//         setData(datas[i++]);
+//         if(i === datas.length) i = 0;
+//     }
+
+
+//     return (
+//         <div className="App">
+//             <h2>Graphs with React</h2>
+//             <button onClick={changeData}>Change Data</button>
+//             <BarChart width={600} height={400} data={data} />
+//         </div>
+//     );
+// }
 
 export default App;
---
+++ src/utils/file.js
@@ -0,0 +1,27 @@
+import { tsv } from 'd3-fetch';
+
+export async function fetchData() {
+    const data = await tsv("data/analysis.txt", (datum) => {
+        return {
+            uri: datum.uri,
+            track_name: datum.track_name,
+            album_name: datum.album_name,
+            artist_name: datum.artist_name,
+            danceability: +datum.danceability,
+            energy: +datum.energy,
+            key: +datum.key,
+            loudness: +datum.loudness,
+            mode: +datum.mode,
+            speechiness: +datum.speechiness,
+            acousticness: +datum.acousticness,
+            instrumentalness: +datum.instrumentalness,
+            liveness: +datum.liveness,
+            valence: +datum.valence,
+            tempo: +datum.tempo,
+            duration_ms: +datum.duration_ms,
+            time_signature: +datum.time_signature,
+        }
+    });
+
+    return data;
+}
